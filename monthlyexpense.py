c657fa4757b587ba62dd520b61ff432c9b1db44d
vikas reddy <48214808+vikasreddykunapally@users.noreply.github.com>
2019-03-11 21:12:02 +0530

    Update and rename app.py to monthlyexpense.py

--- monthlyexpense.py
+++ monthlyexpense.py
@@ -3,8 +3,8 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
-   def list(): 
-return render_template("index.html",show=monthlyexpenses)
+def index(): 
+       return render_template "index.html"
 
 if __name__ == "__main__":   
 app.run()
